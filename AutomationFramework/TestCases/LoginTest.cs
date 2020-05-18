cfda5c4a39aff0d06b1d3deb3240e46d8e67b700
RaulMrl <raul_merlan@yahoo.com>
2020-05-18 17:21:38 +0300

    Commit

---
+++ AutomationFramework/TestCases/LoginTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace AutomationFramework.TestCases
+{    
+    class LoginTest
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://newtours.demoaut.com/";
+            driver.Quit();
+        }
+    }
+}
