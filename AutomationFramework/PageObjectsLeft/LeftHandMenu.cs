25dab3965a5c81694d12764c5be5b71223b4c216
Raul Merlan <Raul.Merlan@Endava.com>
2020-05-20 21:42:27 +0300

    HomeScreen, LeftHandMenu, TopMenu

---
+++ AutomationFramework/PageObjectsLeft/LeftHandMenu.cs
@@ -0,0 +1,21 @@
+﻿using AutomationFramework.PageObjectsLeft;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFramework
+{
+    class LeftHandMenu : PageObject
+    {
+        public IWebElement Home => WebDriver.FindElement(By.LinkText("mercurywelcome.php"));
+        public IWebElement Flights => WebDriver.FindElement(By.LinkText("mercuryreservation.php"));
+        public IWebElement Hotels => WebDriver.FindElement(By.LinkText("mercuryunderconst.php"));
+        public IWebElement CarRentals => WebDriver.FindElement(By.LinkText("mercuryunderconst.php"));
+        public IWebElement Cruises => WebDriver.FindElement(By.LinkText("mercurycruise.php"));
+        public IWebElement Destinations => WebDriver.FindElement(By.LinkText("mercuryunderconst.php"));
+        public IWebElement VacationsElement => WebDriver.FindElement(By.LinkText("mercuryunderconst.php"));
+    }
+}
