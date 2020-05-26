089d2e6303a1016150a226a96be079e1d1d58f95
Raul Merlan <Raul.Merlan@Endava.com>
2020-05-26 17:17:09 +0300

    SignOnPage

---
+++ AutomationFramework/PageObjectsLeft/SignOnPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFramework.PageObjectsLeft
+{
+    class SignOnPage : PageObject
+    {
+        public IWebElement UserNameElement => WebDriver.FindElement(By.Name("userName"));
+        public IWebElement PasswordElement => WebDriver.FindElement(By.Name("password"));
+        public IWebElement SignInElement => WebDriver.FindElement(By.Name("login"));
+        public IWebElement RegistrationFormElement => WebDriver.FindElement(By.PartialLinkText("registration"));
+        public IWebElement BodyElement => WebDriver.FindElement(By.TagName("body"));
+
+        public void LogIn(string userName, string password)
+        {
+            UserNameElement.SendKeys(userName);
+            PasswordElement.SendKeys(password);
+            SignInElement.Submit();
+        }
+
+        public void RegistrationFormLinkClick()
+        {
+            RegistrationFormElement.Click();
+        }
+    }
+}
---
+++ AutomationFramework/TestCases/SignOnPageTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.PageObjects;
+using AutomationFramework.PageObjectsLeft;
+using AutomationFramework.Common;
+
+namespace AutomationFramework.TestCases
+{    
+    class SignOnPageTests
+    {
+        public SignOnPage SignOnPage;
+
+        [SetUp]
+        public void Initialize()
+        {
+            SignOnPage = new SignOnPage();
+            SignOnPage.WebDriver = new ChromeDriver();
+            SignOnPage.WebDriver.Url = "http://newtours.demoaut.com/mercurysignon.php";
+            Console.WriteLine("Opened URL");
+        }
+
+        [Test]
+        public void RegistrationFormLink()
+        {
+            SignOnPage.RegistrationFormLinkClick();
+            Assert.IsTrue(SignOnPage.BodyElement.Text.Contains("account"));
+        }
+
+        [Test]
+        public void LogInWithValidData()
+        {
+            SignOnPage.LogIn("UserNameTest", "pass");
+            Assert.IsTrue(SignOnPage.BodyElement.Text.Contains("Welcome UserNameTest"));
+        }
+
+        [Test]
+        public void LogInWithInvalidData()
+        {
+            SignOnPage.LogIn("invalid", "invalid");
+            Assert.IsTrue(SignOnPage.BodyElement.Text.Contains("User Name"));
+        }
+
+
+        [Test]
+        public void LogInWithNoData()
+        {
+            SignOnPage.LogIn("", "");
+            Assert.IsTrue(SignOnPage.BodyElement.Text.Contains("User Name"));
+        }
+        
+        [TearDown]
+        public void CleanUp()
+        {
+            SignOnPage.WebDriver.Quit();
+            Console.WriteLine("Closed the browser");
+        }
+    }
+}
